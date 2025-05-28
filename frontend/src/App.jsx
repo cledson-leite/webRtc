e41bfcb0aa55321e57eed8a5c9751fa557666f0d
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2025-05-28 17:37:16 -0300

    frontend initial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export const App = () => {
+    return (
+    <>
+      <h1>Hello World!!!</h1>
+    </>
+  )
+}
